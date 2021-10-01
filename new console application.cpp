0ba34091d17d48e6031015487fc126ee3bfd70a5
gargo <gargo@DESKTOP-0FQOTC2>
2021-10-01 22:07:51 +0530

    Add project files.

---
+++ new console application.cpp
@@ -0,0 +1,61 @@
+// new console application.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <string>
+#include "new console application.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+/*
+int power(int base_num, int pow) {
+    int result = 1;
+    for (int i = 0; i < pow; i++) {
+        result = result * base_num;
+    }
+    return result;
+
+}
+
+int main() {
+    int base, exponent;
+    cout << "base:";
+    cin >> base;
+
+    cout << "expo:";
+    cin >> exponent;
+
+    int ojas = power(base, exponent);
+    cout << "the result is ..." << ojas;
+
+    return 0;
+}
+*/
+
+//----------------------------------------------------------
+//void functions : void function have no return value but does the calculation like a simple function.
+
+
+int main() {
+    //intergral data types ----------------------------------------------
+
+   // int i = 9999999999999999999999999999999999999999999999; this is an error because int cannot store this much numbers.
+   // but python can.............. ;))
+
+}  
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
