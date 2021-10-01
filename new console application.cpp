a87843a0b3c55fd2924496d7b703825e5b8ba72b
ojas garg <gargojas31@gmail.com>
2021-10-01 22:23:19 +0530

    new changes bro

--- new console application.cpp
+++ new console application.cpp
@@ -1,5 +1,6 @@
 // new console application.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//this console application has literally no code , and if there is it is commented.
+//i am just testing my commenting skills.
 
 
 #include <iostream>
@@ -32,18 +33,26 @@ int main() {
     cout << "the result is ..." << ojas;
 
     return 0;
+
+    
 }
 */
 
 //----------------------------------------------------------
 //void functions : void function have no return value but does the calculation like a simple function.
+// void oh!yeah(){
 
+//}
 
 int main() {
     //intergral data types ----------------------------------------------
 
    // int i = 9999999999999999999999999999999999999999999999; this is an error because int cannot store this much numbers.
    // but python can.............. ;))
+   // though long can do this if you still want to flex with cpp ;))
+   //here are some data types if you want to know about them so badly:
+   //short , int , long , long long , unsigned short , unsigned it , unsigned long , unsigned long long 
+   // let me tell you what unsigned and signed really is - "
 
 }  
 
@@ -52,10 +61,9 @@ int main() {
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*you can also not download visual studio as it is not too important.You just have to see a tutorial to set up cpp from
+vs code and then you can use "g++ <name of file> (dont use these pointed brackets with file name) and then a a.exe or a.out
+file will be created . Then you can run it by either typing a.exe for .exe file and ./a.out for .out file
+*/  
+
+/* c# tutorial file coming soon....*/
